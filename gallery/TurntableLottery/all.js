a4bd60ebe028460eb7f0cb99731b38133811b68f
Cheesy-Cake <yolo.52099@gmail.com>
2021-08-10 20:36:46 +0800

    edit

--- gallery/TurntableLottery/all.js
+++ gallery/TurntableLottery/all.js
@@ -10,6 +10,7 @@ window.onload = function(){
                         results[results.length] = elems[i];
                     }
                 }
+                console.log(results);
                 return results;
             }
         }
