84edc264d51012ff6a39e3d756b17169f54d4a3c
Cheesy-Cake <yolo.52099@gmail.com>
2021-08-12 22:37:14 +0800

    update

--- gallery/TodoList/js/all.js
+++ gallery/TodoList/js/all.js
@@ -4,3 +4,9 @@
 //變數名稱.addEventListener('狀態',Function);
 //定義一個 function 名稱(){};
 
+const itemsContainerBox = document.getElementById('itemsContainerBox');
+const addBtn = document.getElementById('addBtn');
+addBtn.addEventListener('click',addTask);
+function addTask () {
+  console.log('OK');
+}
