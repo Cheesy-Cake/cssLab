85f786600dc2dcce7100b91119f7ceee4a806331
Cheesy-Cake <yolo.52099@gmail.com>
2021-08-13 11:24:11 +0800

    Update all.js

--- gallery/TodoList/js/all.js
+++ gallery/TodoList/js/all.js
@@ -7,10 +7,12 @@
 const itemsContainerBox = document.getElementById('itemsContainerBox');
 const addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click',addTask);
-let div = document.createElement('div');
 function addTask () {
-  // const itemInput = document.getElementById('itemInput');
-  // const createItem = itemInput.value;
+  const itemInput = document.getElementById('itemInput');
+  const createItem = itemInput.value;
   // itemsContainerBox.append(createItem);
-  itemsContainerBox.appendChild(div);
+  let div = document.createElement('div');
+  let taskText = document.createText(createItem);
+  div.appendChild(taskText);
+  // itemsContainerBox.appendChild(div);
 }
